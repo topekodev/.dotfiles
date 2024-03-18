a84fb724b35aeca8b2fff3ff59736eacd9f6bf32
Tomi Koskinen <tomi@topeko.dev>
2024-03-18 21:09:53 +0200

    Keybindings for buffuers and navigating telescope

--- .config/nvim/lua/topeko/plugins/telescope.lua
+++ .config/nvim/lua/topeko/plugins/telescope.lua
@@ -5,7 +5,20 @@ return {
         'nvim-lua/plenary.nvim'
     },
     config = function()
-        require('telescope').setup({})
+        require('telescope').setup({
+            defaults = {
+                mappings = {
+                    i = {
+                        ["<C-j>"] = require('telescope.actions').move_selection_next,
+                        ["<C-k>"] = require('telescope.actions').move_selection_previous,
+                    },
+                    n = {
+                        ["<j>"] = require('telescope.actions').move_selection_next,
+                        ["<k>"] = require('telescope.actions').move_selection_previous,
+                    }
+                }
+            }
+        })
         local builtin = require('telescope.builtin');
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {});
         vim.keymap.set('n', '<leader>pg', builtin.git_files, {});
--- .config/nvim/lua/topeko/remap.lua
+++ .config/nvim/lua/topeko/remap.lua
@@ -1,2 +1,8 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>,", vim.cmd.bprev)
+vim.keymap.set("n", "<leader>.", vim.cmd.bnext)
+vim.keymap.set("n", "<leader>-", vim.cmd.bdelete)
+vim.keymap.set("n", "<leader>l", function()
+    vim.cmd("Telescope buffers")
+end)
