a098bc79ff7fa879458bb513272740b485fbc50a
Tomi Koskinen <tomi@topeko.dev>
2024-03-19 01:56:54 +0200

    Keybinds for tabs

--- .config/nvim/lua/topeko/remap.lua
+++ .config/nvim/lua/topeko/remap.lua
@@ -1,8 +1,21 @@
 vim.g.mapleader = " "
+
+-- Netrw
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>,", vim.cmd.bprev)
-vim.keymap.set("n", "<leader>.", vim.cmd.bnext)
-vim.keymap.set("n", "<leader>-", vim.cmd.bdelete)
-vim.keymap.set("n", "<leader>l", function()
+
+-- Buffers
+vim.keymap.set("n", "<leader>h", vim.cmd.bprev)
+vim.keymap.set("n", "<leader>l", vim.cmd.bnext)
+vim.keymap.set("n", "<leader>db", vim.cmd.bdelete)
+vim.keymap.set("n", "<leader>pb", function()
     vim.cmd("Telescope buffers")
 end)
+
+-- Tabs
+vim.keymap.set("n", "<leader>nt", vim.cmd.tabe)
+vim.keymap.set("n", "<leader>dt", vim.cmd.tabc)
+for i = 1, 9 do
+    vim.keymap.set("n", "<leader>" .. i, function()
+        vim.cmd("tabn " .. i)
+    end)
+end
